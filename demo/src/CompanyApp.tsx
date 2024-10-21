d16505081916926d9d040e1ef8ff1bfa70097f68
Mark Miro <contact@markmiro.com>
2024-10-20 20:55:56 -0700

    Add matine and autoform

--- demo/src/CompanyApp.tsx
+++ demo/src/CompanyApp.tsx
@@ -1,13 +1,16 @@
 import { z } from 'zod';
 import { BubbleForm } from './bubbler';
 import { companySchema } from './schema';
+import { AutoForm } from '@autoform/mantine';
+import { ZodProvider } from '@autoform/zod';
 
-const submitSchema = companySchema;
-type SubmitType = z.infer<typeof submitSchema>;
+const formSchema = companySchema;
+type FormType = z.infer<typeof formSchema>;
+const schemaProvider = new ZodProvider(formSchema);
 
 export default function CompanyApp() {
-  const onSubmit = (data: SubmitType) => {
-    const validate = submitSchema.safeParse(data);
+  const onSubmit = (data: FormType) => {
+    const validate = formSchema.safeParse(data);
 
     if (!validate.success) {
       console.error('invalid data', validate.error);
@@ -22,6 +25,12 @@ export default function CompanyApp() {
       <BubbleForm className="contents">
         {/* You'd expect submit to be at the BubbleForm but it's not because for submission is only triggered at that level. The actual submission only happens when the data is done bubbling up */}
 
+        <AutoForm
+          schema={schemaProvider}
+          onSubmit={(data, form) => {
+            console.log(data);
+          }}
+        />
         <button type="submit">Submit</button>
       </BubbleForm>
     </>
--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -5,26 +5,30 @@ import './index.css';
 import CompanyApp from './CompanyApp';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './components/ui/tabs';
 import { Card } from './components/card';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="max-w-screen-sm mx-auto p-2">
-      <Tabs defaultValue="simple">
-        <TabsList>
-          <TabsTrigger value="simple">Simple</TabsTrigger>
-          <TabsTrigger value="company">Company</TabsTrigger>
-        </TabsList>
-        <TabsContent value="simple">
-          <Card>
-            <App />
-          </Card>
-        </TabsContent>
-        <TabsContent value="company">
-          <Card>
-            <CompanyApp />
-          </Card>
-        </TabsContent>
-      </Tabs>
-    </div>
+    <MantineProvider>
+      <div className="max-w-screen-sm mx-auto p-2">
+        <Tabs defaultValue="simple">
+          <TabsList>
+            <TabsTrigger value="simple">Simple</TabsTrigger>
+            <TabsTrigger value="company">Company</TabsTrigger>
+          </TabsList>
+          <TabsContent value="simple">
+            <Card>
+              <App />
+            </Card>
+          </TabsContent>
+          <TabsContent value="company">
+            <Card>
+              <CompanyApp />
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </MantineProvider>
   </StrictMode>,
 );
